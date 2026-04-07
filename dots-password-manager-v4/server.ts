fd55507ce1e4e3858af2b3ed1f797307e6365f4c
Giorgio Dodesini <giorgiodotsdev@gmail.com>
2026-04-07 21:37:18 +0200

    setting up build

---
+++ dots-password-manager-v4/server.ts
@@ -0,0 +1,9 @@
+import handler, { createServerEntry } from '@tanstack/react-start/server-entry'
+import { FastResponse } from 'srvx'
+globalThis.Response = FastResponse
+
+export default createServerEntry({
+    fetch(request) {
+        return handler.fetch(request)
+    },
+})
--- dots-password-manager-v4/vite.config.ts
+++ dots-password-manager-v4/vite.config.ts
@@ -3,18 +3,22 @@ import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
-  plugins: [
-    devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+    plugins: [
+        devtools(),
+        tsconfigPaths({ projects: ['./tsconfig.json'] }),
+        tailwindcss(),
+        tanstackStart(),
+        nitro(),
+        viteReact(),
+    ],
+    environments: {
+        ssr: { build: { rollupOptions: { input: './server.ts' } } },
+    },
 })
 
 export default config
